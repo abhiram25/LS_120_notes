d2ae094bfa2988a17629afaafc7b856ad0d88e83
Abhi Reddy <abhi.reddy1@gmail.com>
2016-08-16 09:25:43 -0400

    Update collaborator_objects.rb

--- Lesson2_notes/collaborator_objects.rb
+++ Lesson2_notes/collaborator_objects.rb
@@ -1,1 +1,18 @@
 
+```Ruby
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+end
+
+joe = Person.new("Joe")
+joe.name                    # => "Joe"
+```
+
+**Instance** variables can hold any object, not only strings and integers like the example above.
+
